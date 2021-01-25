86ab58958a9e50a6e9c199f5125db4e90aaabb48
sawfly29 <sawfly@yandex.ru>
2021-01-25 20:59:26 +0300

    Redux basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createStore
+} from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state + action.payload;
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document
+  .getElementById('inc')
+  .addEventListener('click', () => {
+    store.dispatch({
+      type: 'INC'
+    })
+  })
+
+document
+  .getElementById('dec')
+  .addEventListener('click', () => {
+    store.dispatch({
+      type: 'DEC'
+    })
+  })
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    store.dispatch({
+      type: 'RND',
+      payload: Math.floor(Math.random()*10)
+    })
+  })
+
+  const update = () => {
+    document
+    .getElementById('counter')
+    .innerHTML = store.getState()
+  }
+
+  store.subscribe(update)//функция сработает только после изменения state - эдакий листенер
+
+
+//reducer должен быть чистой функцией: 
+//1 - зависит только от аргументов (всегда при одном наборе аргументов будет получаться один рез-т)
+//2 - нет побочных эффектов (изменения внешнего состояния среды относ. функции)
+
+// 
+
+// store.subscribe(() => {console.log(store.getState())})
+
+// store.dispatch({type: 'INC'}) //обработка нового действия
+// store.dispatch({type: 'INC'})
+// store.dispatch({type: 'INC'})
+
+
+// let state = reducer(undefined, {})
+// console.log(state)
+
+// state = reducer(state, {type: 'INC'})
+// console.log(state)
+
+// state = reducer(state, {type: 'INC'})
+// console.log(state)
